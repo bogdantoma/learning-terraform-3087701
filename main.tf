bd829db6a4c3ef7d7d55a06d9523d99b436e98c7
Bogdan Toma <bogdan.toma@gmail.com>
2023-12-19 23:16:50 +0200

    load balancer

--- main.tf
+++ main.tf
@@ -27,6 +27,29 @@ resource "aws_instance" "blog" {
   }
 }
 
+module "blog_alb" {
+  source = "terraform-aws-modules/alb/aws"
+
+  name    = "blog-alb"
+  vpc_id  = module.blog_vpc.vpc_id
+  subnets = module.blog_vpc.public_subnets
+  security_groups = [module.blog_sg.security_group_id]
+  
+  target_groups = {
+    blog-instance = {
+      name_prefix      = "blog-"
+      protocol         = "HTTP"
+      port             = 80
+      target_type      = "instance"
+      target_id = aws_instance.blog.id
+    }
+  }
+
+  tags = {
+    Environment = "dev"
+  }
+}
+
 module "blog_vpc" {
   source = "terraform-aws-modules/vpc/aws"
 
