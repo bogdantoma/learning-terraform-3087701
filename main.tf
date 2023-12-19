b7b03c785487da7033e291d95727ac334cdd033f
Bogdan Toma <bogdan.toma@gmail.com>
2023-12-19 22:55:33 +0200

    vpc module

--- main.tf
+++ main.tf
@@ -19,14 +19,27 @@ resource "aws_instance" "blog" {
   instance_type = var.instance_type
 
   vpc_security_group_ids = [module.blog_sg.security_group_id]
+  
+  subnet_id = module.blog_vpc.public_subnets[0]
+
   tags = {
     Name = "HelloWorld"
   }
 }
 
+module "blog_vpc" {
+  source = "terraform-aws-modules/vpc/aws"
 
-data "aws_vpc" "default"{
-  default = true
+  name = "blog"
+  cidr = "10.0.0.0/16"
+
+  azs             = ["us-west-2a", "us-west-2b", "us-west-2c"]
+  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+
+  tags = {
+    Terraform = "true"
+    Environment = "dev"
+  }
 }
 
 module "blog_sg"{
@@ -34,7 +47,7 @@ module "blog_sg"{
   version = "4.13.0"
   name = "blog"
 
-  vpc_id = data.aws_vpc.default.id
+  vpc_id = module.blog_vpc.vpc_id
 
   ingress_rules = ["http-80-tcp", "https-443-tcp"]
   ingress_cidr_blocks = ["0.0.0.0/0"]
